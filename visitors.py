547a075af2196a2c1827b6e86a79abc72b4c391a
Fabian665 <71074539+Fabian665@users.noreply.github.com>
2023-05-17 16:11:08 +0300

    functions for everybody

--- visitors.py
+++ visitors.py
@@ -2,5 +2,55 @@ from utils import append_decorator
 
 
 @append_decorator
-def base_func() -> str:
+def barak() -> str:
+    return ""
+
+
+@append_decorator
+def beni() -> str:
+    return ""
+
+
+@append_decorator
+def danielle() -> str:
+    return ""
+
+
+@append_decorator
+def idan() -> str:
+    return ""
+
+
+@append_decorator
+def mark() -> str:
+    return ""
+
+
+@append_decorator
+def maya() -> str:
+    return ""
+
+
+@append_decorator
+def nikolai() -> str:
+    return ""
+
+
+@append_decorator
+def _or() -> str:
+    return ""
+
+
+@append_decorator
+def yuval_c() -> str:
+    return ""
+
+
+@append_decorator
+def yuval_g() -> str:
+    return ""
+
+
+@append_decorator
+def yuval_l() -> str:
     return ""
